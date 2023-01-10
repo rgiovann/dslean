b3b5599ab61748d339969db5a55385dafb10dad3
Giovani L. Rozza <giovanni.rozza@gmail.com>
2023-01-10 15:16:35 -0300

    Created Course entity.

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://www.publicdomainpictures.net/pictures/270000/nahled/training-course-training-online.jpg','https://vle.btvi.edu.bs/moodle/pluginfile.php/101066/course/overviewfiles/HTML-00-CourseImage.jpg');
