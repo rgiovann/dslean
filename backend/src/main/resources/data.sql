a4185d3a932a958dd2d943c776184649a73349ff
Giovani L. Rozza <giovanni.rozza@gmail.com>
2023-01-11 15:47:02 -0300

    Enrollment seed.

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -25,3 +25,6 @@ INSERT INTO tb_resource (title, description,position,img_Uri,type,offer_id) VALU
 INSERT INTO tb_section (title, description,position,img_Uri,resource_id, prerequisite_id) VALUES ('Capítulo 01','Neste capítulo vamos começar...',1,'https://openclipart.org/image/800px/69331',1,null );
 INSERT INTO tb_section (title, description,position,img_Uri,resource_id, prerequisite_id) VALUES ('Capítulo 02','Neste capítulo vamos continuar...',2,'https://openclipart.org/image/800px/69331',1,1 );
 INSERT INTO tb_section (title, description,position,img_Uri,resource_id, prerequisite_id) VALUES ('Capítulo 03','Neste capítulo vamos finalizar...',3,'https://openclipart.org/image/800px/69331',1,2 );
+
+INSERT INTO tb_enrollment (user_id, offer_id,enroll_Moment,refund_Moment, available, only_Update) VALUES (1, 1,TIMESTAMP WITH TIME ZONE '2022-09-14T13:00:00Z',null,true,false);
+INSERT INTO tb_enrollment (user_id, offer_id,enroll_Moment,refund_Moment, available, only_Update) VALUES (2, 1,TIMESTAMP WITH TIME ZONE '2022-09-15T15:00:00Z',null,true,false);
